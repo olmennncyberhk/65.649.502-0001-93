546371945a3b8d140a980f3dff54a9248c8a9a8e
insane <a84771426@gmail.com>
2026-06-29 04:15:03 -0300

    removendo arquivos

--- js/config.example.js
+++
@@ -1,3 +0,0 @@
-// Copie para site-config.js e ajuste:
-// apiUrl: 'https://geragerapiuxis.top/api-pescabrasil'
-// whatsappSuporte: '55...'
--- js/config.js
+++
@@ -1,2 +0,0 @@
-// API: edite js/site-config.js → apiUrl
-// WhatsApp: edite js/site-config.js → whatsappSuporte
